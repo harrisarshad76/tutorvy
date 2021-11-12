b1151c46e7e8f669ba388626370068776cd3b6c1
MohsinMalik2 <malikmuhsin70@gmail.com>
2021-11-12 20:23:44 +0500

    profile professional

--- resources/views/tutor/pages/profile/index.blade.php
+++ resources/views/tutor/pages/profile/index.blade.php
@@ -920,7 +920,7 @@
                                                                     <option value="2019">2019</option>
                                                                     <option value="2020">2020</option>
                                                                     <option value="2021">2021</option>
-                                                                    <option value="202s2">2022</option>
+                                                                    <option value="2022">2022</option>
                                                                     <option value="2023">2023</option>
                                                                 </select>
                                                         </div>
@@ -953,7 +953,7 @@
                                                                     <option value="2019">2019</option>
                                                                     <option value="2020">2020</option>
                                                                     <option value="2021">2021</option>
-                                                                    <option value="202s2">2022</option>
+                                                                    <option value="2022">2022</option>
                                                                     <option value="2023">2023</option>
                                                                 </select>
 
@@ -1027,7 +1027,7 @@
                                                                             <option value="2019">2019</option>
                                                                             <option value="2020">2020</option>
                                                                             <option value="2021">2021</option>
-                                                                            <option value="202s2">2022</option>
+                                                                            <option value="2022">2022</option>
                                                                             <option value="2023">2023</option>
                                                                         </select>
 
@@ -1062,7 +1062,7 @@
                                                                             <option value="2019">2019</option>
                                                                             <option value="2020">2020</option>
                                                                             <option value="2021">2021</option>
-                                                                            <option value="202s2">2022</option>
+                                                                            <option value="2022">2022</option>
                                                                             <option value="2023">2023</option>
                                                                         </select>
                                                                 </div>
