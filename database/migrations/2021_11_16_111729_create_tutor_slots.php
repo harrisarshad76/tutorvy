f60404ab2df1d7e801227ea6d9ff7b9064f3803e
MuzammilNaveed <muzamilnaveed10@gmail.com>
2021-11-16 18:06:03 +0500

    update

---
+++ database/migrations/2021_11_16_111729_create_tutor_slots.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTutorSlots extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tutor_slots', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->nullable();
+            $table->string('day')->nullable();
+            $table->string('wrk_from')->nullable();
+            $table->string('wrk_to')->nullable();
+            $table->string('slot_length')->nullable();
+            $table->string('bk_pr_slot')->nullable();
+            $table->string('day_off')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutor_slots');
+    }
+}
