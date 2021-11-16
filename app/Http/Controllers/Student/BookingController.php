0487d818f429536208fa7448110d18f71ad913cc
Muhammad Kashif <muhammadkashif70000@gmail.com>
2021-11-16 17:34:55 +0500

    reschedule booking changes

--- app/Http/Controllers/Student/BookingController.php
+++ app/Http/Controllers/Student/BookingController.php
@@ -806,6 +806,23 @@ class BookingController extends Controller
         $booking->class_time = $request->time;
         $booking->reschedule_note = $request->note;
         $booking->save();
+
+        $admin = User::where('role',1)->first();
+        $name = Auth::user()->first_name . ' ' . Auth::user()->last_name;
+        $notification = new NotifyController();
+        $slug = URL::to('/') . '/tutor/booking-detail/' . $booking->id;
+        $type = 'booking_rescheduled';
+        $title = 'Booking Rescheduled';
+        $icon = 'fas fa-tag';
+        $class = 'btn-success';
+        $desc = $name . ' Rescheduled the booking.';
+        $pic = Auth::User()->picture;
+        $notification->GeneralNotifi($booking->booked_tutor ,$slug,$type,$title,$icon,$class,$desc,$pic);
+
+        // send to admin
+        $admin_slug = URL::to('/') . '/admin/booking-detail/' . $booking->id;
+        $notification->GeneralNotifi($admin->id,$admin_slug,$type,$title,$icon,$class,$desc,$pic);
+
         return response()->json([
             "message" => "Meeting Rescheduled Successfully",
             "status_code" => 200,
