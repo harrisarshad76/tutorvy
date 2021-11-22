c1a8109e7ca9a2959d8a4fd6600c44b68b11ddc4
Muhammad Kashif <muhammadkashif70000@gmail.com>
2021-11-22 17:49:23 +0500

    changes

--- app/Http/Controllers/General/NotifyController.php
+++ app/Http/Controllers/General/NotifyController.php
@@ -89,7 +89,7 @@ class NotifyController extends Controller
                 $user->save();
               }else{
                 array_push($fcm_array, $fcm_data);
-                $user->token = $fcm_data;
+                $user->token = $fcm_array;
                 $user->token_updated_at = date('Y-m-d h:m:s');
                 $user->is_token_updated = 1;
                 $user->save();
