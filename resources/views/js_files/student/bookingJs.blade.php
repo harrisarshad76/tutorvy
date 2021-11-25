4138e20a09603a4d586fac83e5fd0474fbdfecdf
Muhammad Kashif <muhammadkashif70000@gmail.com>
2021-11-25 19:12:36 +0500

    changes

--- resources/views/js_files/student/bookingJs.blade.php
+++ resources/views/js_files/student/bookingJs.blade.php
@@ -342,13 +342,14 @@ function pay_now(id) {
 
                 var origin   = window.location.origin;
                 var url = origin + '/student/booking/payment/'+ obj.id;
-                $("#show_pay_btn #payment span").html(btn);
+           
 
                 $("#payment").attr("action",url)
                 let btn = `<input type="submit" class="schedule-btn btn w-30" value="Pay Now" />`;
                 $("#show_pay_btn #payment span").html(btn);
 
                 $("#payModel").modal('show');
+                $("#paypal-button-container").html('');
 
                 paypal.Button.render({
                     // selection for sandbox or production
