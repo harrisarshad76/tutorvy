68725b3bee277e9a7b4568de5838241589ba6856
MohsinMalik2 <malikmuhsin70@gmail.com>
2021-11-29 18:43:15 +0500

    previous date disablity

--- resources/views/js_files/student/tutorJs.blade.php
+++ resources/views/js_files/student/tutorJs.blade.php
@@ -10,7 +10,8 @@ $(document).ready(function() {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-
+        var today = new Date().toISOString().split('T')[0];
+        document.getElementsByName("date")[0].setAttribute('min', today);
 
 });
 
