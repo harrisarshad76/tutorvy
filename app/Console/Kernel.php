2d8aa2e6619f2a5ee62fb894952d88331494d687
Muhammad Kashif <muhammadkashif70000@gmail.com>
2021-12-07 19:48:48 +0500

    cron changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('generate:payouts')->everyMinute();
-        // $schedule->command('command:check_class_status')->everyMinute();
+        $schedule->command('command:check_class_status')->everyMinute();
     }
 
     /**
